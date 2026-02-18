71bb53072b791e9be65b1b6017f590ba6e4f587a
sebb3 <mail@sebbe.cc>
2026-02-18 12:04:34 +0100

    add linux support

--- flake.nix
+++ flake.nix
@@ -3,22 +3,31 @@
     url = "https://github.com/NixOS/nixpkgs/archive/refs/heads/nixos-23.05.tar.gz";
     flake = false;
   };
+
   outputs =
-    { nixpkgs,self, ... }:
+    { nixpkgs, ... }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      lib = (import nixpkgs { system = "x86_64-linux"; }).lib;
+      pkgsFor =
+        system:
+        import nixpkgs {
+          inherit system;
+          config.permittedInsecurePackages = [ "nodejs-16.20.2" ];
+        };
+    in
     {
-      overlays.default =
-      let pkgs = p: import nixpkgs {
-        inherit (p.stdenv) system;
-        config = {
-          permittedInsecurePackages = [
-            "nodejs-16.20.2"
-          ];
-        };
+      overlays.default = final: prev: {
+        nodejs_16 = (pkgsFor prev.stdenv.system).nodejs_16;
       };
-      in
-        final: prev: {
-          nodejs_16 = (pkgs prev).nodejs_16;
-        };
-    packages.aarch64-darwin.default = (import nixpkgs { system = "aarch64-darwin"; overlays = [ self.overlays.default ]; }).nodejs_16;
+
+      packages = lib.genAttrs systems (system: {
+        default = (pkgsFor system).nodejs_16;
+      });
     };
 }
